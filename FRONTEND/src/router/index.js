28560c36648c3509b26f7b9b482fbc09f60e0f02
andres859 <kacq262007@gmail.com>
2026-09-19 11:22:10 -0500

    c

--- FRONTEND/src/router/index.js
+++ FRONTEND/src/router/index.js
@@ -31,9 +31,9 @@ const routes = [
   { path: '/cliente/cuenta',   name: 'ClienteCuenta',   component: () => import('../views/cliente/CuentaPersonal.vue'),   meta: { rol: 'cliente' } },
   { path: '/cliente/pedidos',  name: 'ClientePedidos',  component: () => import('../views/cliente/PedidosEntregas.vue'),  meta: { rol: 'cliente' } },
   { path: '/cliente/soporte',  name: 'ClienteSoporte',  component: () => import('../views/cliente/CentroSoporte.vue'),    meta: { rol: 'cliente' } },
-  { path: '/cliente/google-calendar', name: 'ClienteGoogleCalendar', component: () => import('../views/GoogleCalendarView.vue'), meta: { rol: 'cliente' } },
   { path: '/cliente/alertas', name: 'ClienteAlertas', component: () => import('../views/cliente/AlertasMateriales.vue'), meta: { rol: 'cliente' } },
-
+  { path: '/cliente/google-calendar', name: 'ClienteGoogleCalendar', component: () => import('../views/GoogleCalendarView.vue'), meta: { rol: 'cliente' } },
+ 
   // 404
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
